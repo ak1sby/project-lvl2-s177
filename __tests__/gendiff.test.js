dfa936f74b3da7ad0dd53d6999226defd0d26a64
ak1s <ak1s@mail.ru>
2018-01-11 10:58:30 +0300

    Comparison files (JSON,YML, INI)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,15 +1,63 @@
+// import fs from 'fs';
 import gendiff from '../src';
-import expected from './Ignore/expected';
 
-const firstJSONFile = './__tests__/__fixtures__/JSON/firstFile.json';
-const secondJSONFile = './__tests__/__fixtures__/JSON/secondFile.json';
+const basePath = '__tests__/__fixtures__/';
+// const expected = fs.readFileSync(`${basePath}expected`, 'utf8');
+// const expectedRecursive = fs.readFileSync(`${basePath}expectedRecursive`, 'utf8');
 
-const firstYMLFile = './__tests__/__fixtures__/YML/firstFile.yml';
-const secondYAMLFile = './__tests__/__fixtures__/YML/secondFile.yaml';
+const firstJSONFile = `${basePath}JSON/firstFile.json`;
+const secondJSONFile = `${basePath}JSON/secondFile.json`;
 
-const firstINIFile = './__tests__/__fixtures__/INI/firstFile.ini';
-const secondINIFile = './__tests__/__fixtures__/INI/secondFile.ini';
+const firstYMLFile = `${basePath}YML/firstFile.yml`;
+const secondYAMLFile = `${basePath}YML/secondFile.yaml`;
 
+const firstINIFile = `${basePath}INI/firstFile.ini`;
+const secondINIFile = `${basePath}INI/secondFile.ini`;
+
+const firstJSONFileR = `${basePath}JSON/firstFileRecursive.json`;
+const secondJSONFileR = `${basePath}JSON/secondFileRecursive.json`;
+
+const firstYMLFileR = `${basePath}YML/firstFileRecursive.yml`;
+const secondYAMLFileR = `${basePath}YML/secondFileRecursive.yaml`;
+
+const firstINIFileR = `${basePath}INI/firstFileRecursive.ini`;
+const secondINIFileR = `${basePath}INI/secondFileRecursive.ini`;
+
+
+const expected = `{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+}`;
+
+const expectedRecursive = `{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+        setting6: {
+            key: value
+          + ops: vops
+        }
+      + setting4: blah blah
+      + setting5: {
+          + key5: value5
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+      - abc: 12345
+    }
+  + group3: {
+      + fee: 100500
+    }
+}`;
 
 test('test, difference JSON files', () => {
   expect(gendiff(firstJSONFile, secondJSONFile)).toBe(expected);
@@ -21,4 +69,16 @@ test('test, difference YML files', () => {
 
 test('test, difference INI files', () => {
   expect(gendiff(firstINIFile, secondINIFile)).toBe(expected);
+});
+
+test('test, difference JSON Recursive files', () => {
+  expect(gendiff(firstJSONFileR, secondJSONFileR)).toBe(expectedRecursive);
+});
+
+test('test, difference YML Recursive files', () => {
+  expect(gendiff(firstYMLFileR, secondYAMLFileR)).toBe(expectedRecursive);
+});
+
+test('test, difference INI Recursive files', () => {
+  expect(gendiff(firstINIFileR, secondINIFileR)).toBe(expectedRecursive);
 });
--- src/index.js
+++ src/index.js
@@ -19,25 +19,113 @@ const getObject = (file) => {
   return parsers[extension] ? parsers[extension](fileToString) : '';
 };
 
+const getIndent = (depthNum, prefixSapce, indent) => {
+  if (depthNum === 0) {
+    return prefixSapce;
+  }
+  return getIndent(depthNum - 1, `${prefixSapce}${indent}`, indent);
+};
+
+const genObj = (data) => {
+  if (data instanceof Object) {
+    return data;
+  }
+  return {};
+};
+
+const getPrefix = (data1, data2) => {
+  if (data1 instanceof Object && data2 instanceof Object) {
+    return '  ';
+  }
+  if (data1 instanceof Object) {
+    return '- ';
+  }
+  return '+ ';
+};
+
+const genDiffParse = (firstObj, secondObj, depth = 0) => {
+  const firstFileKeys = Object.keys(firstObj);
+  const secondFileKeys = Object.keys(secondObj);
+  const uniqKeys = _.union(firstFileKeys, secondFileKeys);
+  return uniqKeys.map((key) => {
+    const value1 = firstObj[key];
+    const value2 = secondObj[key];
+    if ((value1 instanceof Object) || (value2 instanceof Object)) {
+      return {
+        type: 'haschild',
+        key,
+        children: genDiffParse(genObj(value1), genObj(value2), depth + 1),
+        prefix: getPrefix(value1, value2),
+        depthNum: depth,
+      };
+    }
+    if (value1 === value2) {
+      return {
+        type: 'original',
+        key,
+        value: value1,
+        prefix: '  ',
+        depthNum: depth,
+      };
+    }
+    if ((key in firstObj) && (key in secondObj)) {
+      return {
+        type: 'changed',
+        key,
+        old: value1,
+        oldPrefix: '- ',
+        new: value2,
+        newPrefix: '+ ',
+        depthNum: depth,
+      };
+    }
+    if (key in secondObj) {
+      return {
+        type: 'added',
+        key,
+        value: value2,
+        prefix: '+ ',
+        depthNum: depth,
+      };
+    }
+    return {
+      type: 'deleted',
+      key,
+      value: value1,
+      prefix: '- ',
+      depthNum: depth,
+    };
+  });
+};
+
+const render = (data) => {
+  const resultArray = data.reduce((acc, obj) => {
+    const key = obj.key ? obj.key : '';
+    const prefixSpace = '  ';
+    const indent = getIndent(obj.depthNum, prefixSpace, '    ');
+    if (obj.type === 'haschild') {
+      return [...acc, `${indent}${obj.prefix}${key}: {${render(obj.children)}${indent}${prefixSpace}}`];
+    }
+    if (obj.type === 'original') {
+      return [...acc, `${indent}${obj.prefix}${key}: ${obj.value}`];
+    }
+    if (obj.type === 'changed') {
+      return [...acc, `${indent}${obj.newPrefix}${key}: ${obj.new}\n${indent}${obj.oldPrefix}${key}: ${obj.old}`];
+    }
+    if (obj.type === 'added') {
+      return [...acc, `${indent}${obj.prefix}${key}: ${obj.value}`];
+    }
+    return [...acc, `${indent}${obj.prefix}${key}: ${obj.value}`];
+  }, []);
+  return `\n${resultArray.join('\n')}\n`;
+};
+
 const gendiff = (firstfile, secondfile) => {
   const firstObject = getObject(firstfile);
   const secondObject = getObject(secondfile);
-  const firstFileKeys = Object.keys(firstObject);
-  const secondFileKeys = Object.keys(secondObject);
-  const uniqKeys = _.union(firstFileKeys, secondFileKeys);
-  const resultArray = uniqKeys.reduce((acc, key) => {
-    if (key in firstObject && key in secondObject) {
-      if (firstObject[key] === secondObject[key]) {
-        return [...acc, `  ${key}: ${firstObject[key]}`];
-      }
-      return [...acc, `+ ${key}: ${secondObject[key]}\n- ${key}: ${firstObject[key]}`];
-    }
-    if (key in firstObject && !(key in secondObject)) {
-      return [...acc, `- ${key}: ${firstObject[key]}`];
-    }
-    return [...acc, `+ ${key}: ${secondObject[key]}`];
-  }, []);
-  return `{\n${resultArray.join('\n')}\n}`;
+  // console.log(JSON.stringify(genDiffParse(firstObject, secondObject), null, '  '));
+  const result = render(genDiffParse(firstObject, secondObject));
+  return `{${result}}`;
 };
 
 export default gendiff;
