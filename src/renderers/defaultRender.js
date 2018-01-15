94eb393f98b0702078d52a991fd965c9dbe36399
ak1s <ak1s@mail.ru>
2018-01-15 13:14:17 +0300

    refactoring

--- src/renderers/defaultRender.js
+++ src/renderers/defaultRender.js
@@ -12,8 +12,8 @@ const defaultRender = (data, curentIndent = setIndent) => {
     const objectToString = (obj) => {
       const result = Object.keys(obj)
         .map(key =>
-          `${constIndent}${indent}${prefixSpace}${key}: ${_.isObject(obj[key]) ? objectToString(obj[key]) : obj[key]}`);
-      return (['{', ...result, `${convertToSpaces(curentIndent)}}`]).join('\n');
+          `{\n${constIndent}${indent}${prefixSpace}${key}: ${_.isObject(obj[key]) ? objectToString(obj[key]) : obj[key]}\n${indent}${prefixSpace}}`);
+      return result;
     };
     return (_.isObject(value) ? `${objectToString(value)}` : `${value}`);
   };
--- src/renderers/plainRender.js
+++ src/renderers/plainRender.js
@@ -4,17 +4,17 @@ const plainRender = (data, parent = '') => {
   const paretnPath = parent.slice(0, parent.length);
 
   const selectFn = {
-    nested: node => [`${plainRender(node.children, `${parent}${node.name}.`)}`],
+    nested: node => `${plainRender(node.children, `${parent}${node.name}.`)}`,
     original: () => '',
-    updated: node => [`Property '${paretnPath}${node.name}' was updated. From '${node.valueBefore}' to '${node.valueAfter}'`],
-    added: node => [`Property '${paretnPath}${node.name}' was added with ${_.isObject(node.valueAfter) ?
-      'complex value' : `value: '${node.valueAfter}'`}`],
-    removed: node => [`Property '${paretnPath}${node.name}' was removed`],
+    updated: node => `Property '${paretnPath}${node.name}' was updated. From '${node.valueBefore}' to '${node.valueAfter}'`,
+    added: node => `Property '${paretnPath}${node.name}' was added with ${_.isObject(node.valueAfter) ?
+      'complex value' : `value: '${node.valueAfter}'`}`,
+    removed: node => `Property '${paretnPath}${node.name}' was removed`,
   };
 
   const result = data.map(node => selectFn[node.type](node));
   const resultArray = _.compact(result);
-  return [...resultArray].join('\n');
+  return resultArray.join('\n');
 };
 
 export default plainRender;
